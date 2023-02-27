e3c3fc485540da06adc31c6e42cde164fc01dfd0
Mariana Correa <mpcs270391@gmail.com>
2023-02-27 15:38:41 -0300

    Exercicios If/else 7

--- fundamentos/secao-03-introducao-a-javascript/exercicios.js
+++ fundamentos/secao-03-introducao-a-javascript/exercicios.js
@@ -160,3 +160,13 @@ if (a % 2 == 0 || b % 2 == 0 || c % 2 == 0) {
 } else {
     console.log(false);
 }
+
+/* 
+Utilize if/else para escrever um código que defina três números em variáveis e retorne true se pelo menos uma das três for ímpar. Caso contrário, o código deve retornar false.
+Bônus: use somente um if.
+*/
+if (a % 2 !== 0 || b % 2 !== 0 || c % 2 !== 0) {
+    console.log(true);
+} else {
+    console.log(false);
+}
