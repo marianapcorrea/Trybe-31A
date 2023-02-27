cb6d377fdb24d4b785cf052983d3fc46244a3ad0
Mariana Correa <mpcs270391@gmail.com>
2023-02-27 14:27:55 -0300

    Exercicios If/else 4

--- fundamentos/secao-03-introducao-a-javascript/exercicios.js
+++ fundamentos/secao-03-introducao-a-javascript/exercicios.js
@@ -53,3 +53,24 @@ if (d < 0) {
 } else {
     console.log("Zero");
 }
+
+/* 
+ Utilize if/else para escrever um código que defina três variaveis com os valores dos três ângulos internos de um triângulo. Retorne true se os ângulos representarem os ângulos de um triângulo e false, caso contrário. Se algum ângulo for inválido, você deve retornar uma mensagem de erro.
+Para os ângulos serem de um triângulo válido, a soma dos três deve ser 180 graus.
+
+Um ângulo será considerado inválido se não tiver um valor positivo.
+
+*/
+
+let angA = 20;
+let angB = 110;
+let angC = -40;
+
+if (angA + angB + angC == 180 && angA > 0 && angB > 0 && angC > 0) {
+    console.log("True");
+}
+if (angA + angB + angC !== 180 && angA > 0 && angB > 0 && angC > 0) {
+    console.log("False");
+} else {
+    console.log("Erro: insira um valor positivo");
+}
