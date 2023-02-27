543f7741f13bc4b8a557d0394dd18269e463aacb
Mariana Correa <mpcs270391@gmail.com>
2023-02-27 14:12:45 -0300

    Exercicios If/else 2

--- fundamentos/secao-03-introducao-a-javascript/exercicios.js
+++ fundamentos/secao-03-introducao-a-javascript/exercicios.js
@@ -1,4 +1,9 @@
 console.log("It's alive!");
+
+let a = 15;
+let b = 9;
+let c = 21;
+
 /* 
 Elabore alguns códigos e imprima o resultado no console usando o console.log, um para cada operação aritmética básica. Seu código deve ter duas variáveis, a e b, definidas no começo com os valores que serão operados. Escreva códigos para:
 Adição (a + b)
@@ -7,8 +12,6 @@ Multiplicação (a * b)
 Divisão (a / b)
 Módulo (a % b)
  */
-let a = 15;
-let b = 9;
 
 console.log("Adição = " + (a + b));
 console.log("Subtração = " + (a - b));
@@ -24,4 +27,16 @@ if (a > b) {
     console.log(a);
 } else {
     console.log(b);
+}
+
+/*
+Utilize if/else para escrever um código que retorne o maior de três números. Defina, no começo do seu código, três variáveis com os valores que serão comparados. 
+*/
+if (a > b && a > c) {
+    console.log(a);
+}
+if (b > a && b > c) {
+    console.log(b);
+} else {
+    console.log(c);
 }
