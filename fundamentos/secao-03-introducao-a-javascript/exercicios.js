3ebbf3fedb21ea8173f6bc114aa37bd3a1772260
Mariana Correa <mpcs270391@gmail.com>
2023-02-27 15:34:27 -0300

    Exercicios If/else 5

--- fundamentos/secao-03-introducao-a-javascript/exercicios.js
+++ fundamentos/secao-03-introducao-a-javascript/exercicios.js
@@ -122,3 +122,31 @@ switch (peca_lowerCase) {
     default:
         console.log("Digite um nome de peça válido.");
 }
+
+/* 
+Utilize if/else para escrever um código que converta uma nota dada em porcentagem (de 0 a 100) em conceitos de A a F. Siga as seguintes regras:
+Porcentagem >= 90 -> A
+Porcentagem >= 80 -> B
+Porcentagem >= 70 -> C
+Porcentagem >= 60 -> D
+Porcentagem >= 50 -> E
+Porcentagem < 50 -> F
+O código deve retornar uma mensagem de erro e encerrar se a nota passada for menor que 0 ou maior que 100.
+*/
+let porcentagem = -200;
+
+if (porcentagem < 0 || porcentagem > 100) {
+    console.log("Nota inválida");
+} else if (porcentagem >= 90) {
+    console.log(" A");
+} else if (porcentagem >= 80) {
+    console.log(" B");
+} else if (porcentagem >= 70) {
+    console.log(" C");
+} else if (porcentagem >= 60) {
+    console.log(" D");
+} else if (porcentagem >= 50) {
+    console.log(" E");
+} else if (porcentagem < 50) {
+    console.log("F)");
+}
