301fa90add3640e92a4e15293f619c64c00dbede
Mariana Correa <mpcs270391@gmail.com>
2023-02-27 13:58:47 -0300

    Exercicios Operadores Matematicos

---
+++ fundamentos/secao-03-introducao-a-javascript/exercicios.js
@@ -0,0 +1,17 @@
+console.log("It's alive!");
+/* 
+Elabore alguns códigos e imprima o resultado no console usando o console.log, um para cada operação aritmética básica. Seu código deve ter duas variáveis, a e b, definidas no começo com os valores que serão operados. Escreva códigos para:
+Adição (a + b)
+Subtração (a - b)
+Multiplicação (a * b)
+Divisão (a / b)
+Módulo (a % b)
+ */
+let a = 15;
+let b = 9;
+
+console.log("Adição = " + (a + b));
+console.log("Subtração = " + (a - b));
+console.log("Multiplicação = " + a * b);
+console.log("Divisão = " + a / b);
+console.log("Resto = " + (a % b));
