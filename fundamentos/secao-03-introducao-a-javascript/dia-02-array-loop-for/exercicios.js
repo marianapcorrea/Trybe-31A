8b7514675fa7cb881d5f687094b17ce350ef7fe2
Mariana Correa <mpcs270391@gmail.com>
2023-02-28 20:32:17 -0300

    Exercicio 11

--- fundamentos/secao-03-introducao-a-javascript/dia-02-array-loop-for/exercicios.js
+++ fundamentos/secao-03-introducao-a-javascript/dia-02-array-loop-for/exercicios.js
@@ -86,10 +86,18 @@ for (i = number; i > 0; i -= 1) {
 }
 console.log(fatorial);
  */
+
 //Utilize a estrutura de repetição for para desenvolver um algoritmo que seja capaz de inverter uma palavra, como a palavra “banana” para “ananab”. Utilize a string abaixo como exemplo. Depois, troque-a por outras para verificar se seu algoritmo está funcionando corretamente.
+let word = "tryber";
+let reordenedWord = "";
+
+for (let i = 1; i < word.length; i += 1) {
+    reordenedWord += word[word.length - 1 - i];
+    console.log(reordenedWord);
+}
 
 //Utilize a estrutura de repetição for para escrever dois algoritmos: um que imprima no console a maior palavra desse array e outro que imprima a menor. Considere o número de caracteres de cada palavra.
-let array = ["java", "javascript", "python", "html", "css"];
+/* let array = ["java", "javascript", "python", "html", "css"];
 let biggestWord = array[0];
 let smallestWord = array[0];
 
@@ -105,3 +113,4 @@ for (let word of array) {
 }
 console.log(biggestWord);
 console.log(smallestWord);
+ */
