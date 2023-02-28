155936d91a37f5ba64435bc30b5f075ebf61d7a4
Mariana Correa <mpcs270391@gmail.com>
2023-02-28 18:58:37 -0300

    Exercicio 4

--- fundamentos/secao-03-introducao-a-javascript/dia-02-array-loop-for/exercicios.js
+++ fundamentos/secao-03-introducao-a-javascript/dia-02-array-loop-for/exercicios.js
@@ -22,10 +22,14 @@ for (let number of numbers) {
     numbers_sum += number;
 }
 numbers_media = numbers_sum / numbers.length;
-console.log(numbers_media);
-
-//A média aritmética é o resultado da soma de todos os elementos dividido pelo número total de elementos.
+/* console.log(numbers_media);
+ */
 //Com base no código que acabou de gerar, faça com que: caso o valor final seja maior que 20, imprima a mensagem “Valor maior que 20”; e, caso não seja maior que 20, imprima a mensagem “Valor menor ou igual a 20”.
+if (numbers_media > 20) {
+    console.log("Valor maior que 20");
+} else {
+    console.log("Valor menor ou igual a 20");
+}
 
 //Utilizando for, descubra o maior valor contido no array e imprima-o.
 
