360cd8cc1a37ccc8c5392b12f7be1818a566ca17
Mariana Correa <mpcs270391@gmail.com>
2023-03-01 15:49:44 -0300

    Content exercise

--- fundamentos/secao-03-introducao-a-javascript/dia-03-funcoes/content-exercises.js
+++ fundamentos/secao-03-introducao-a-javascript/dia-03-funcoes/content-exercises.js
@@ -2,7 +2,7 @@ console.log("It's alive!");
 /* Desenvolva cada exercício a seguir com o uso de funções.
 
 Considere a variável saldo, que representa a quantia em conta de determinada(o) cliente do TrybeBank e escreva quatro programas que: */
-let balance = 3000.0;
+/* let balance = 3000.0;
 let value = 300;
 //Adicione um valor ao saldo.
 function sum(balance, value) {
@@ -14,10 +14,10 @@ sum(balance, value);
 function subtraction(balance, value) {
     console.log(balance - value);
 }
-subtraction(balance, value);
+subtraction(balance, value); */
 
 //Multiplique o valor do saldo por uma taxa.
-function multiplication(balance, value) {
+/* function multiplication(balance, value) {
     console.log(balance * value);
 }
 multiplication(balance, value);
@@ -27,3 +27,20 @@ function division(balance, value) {
     console.log(balance / value);
 }
 division(balance, value);
+ */
+
+//Faça um programa para adicionar clientes ao array de clientes do TrybeBank. Certifique-se de que a função deve receber um parâmetro do tipo string e imprimir uma mensagem de erro caso o parâmetro não seja do tipo string.
+let clientesTrybeBank = ["Ada", "John", "Gus"];
+
+function newClient(client) {
+    if (typeof client === "string") {
+        clientesTrybeBank.push(client);
+        return clientesTrybeBank;
+    } else {
+        return "Type Error: Yous should only use the string type.";
+    }
+}
+console.log(newClient("Beta"));
+console.log(newClient(1));
+
+//Agora iremos escrever um programa que irá excluir um(a) cliente já existente do array de clientes do TrybeBank. Certifique-se de que a função deve receber um parâmetro do tipo string e imprimir uma mensagem de erro, caso o parâmetro não seja do tipo string e caso o(a) cliente não exista dentro do array.
