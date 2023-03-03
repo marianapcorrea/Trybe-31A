8bacf0acfacf2367aa72b27f873806cf9ed2163a
Mariana Correa <mpcs270391@gmail.com>
2023-03-03 19:17:32 -0300

    Bonus exercise

--- fundamentos/secao-03-introducao-a-javascript/dia-05-let-const-arrow-templateLiterals/exercises.js
+++ fundamentos/secao-03-introducao-a-javascript/dia-05-let-const-arrow-templateLiterals/exercises.js
@@ -65,7 +65,7 @@ const speedCar = (speed) =>
 console.log(speedCar(speed)); */
 
 // 1 - Crie a função ligarDesligar, que ligue e desligue um motor de um carro.
-
+/* 
 let ligarDesligar = (status) => {
     let changedStatus = status !== "ligado" ? "ligado" : "desligado";
     console.log(`O motor está ${changedStatus}`);
@@ -92,3 +92,52 @@ let longestWord = (phrase) => {
     }
     return biggestWord;
 };
+ */
+
+//Bônus
+/* 
+Crie duas funções JavaScript com as seguintes especificações:
+Não se esqueça de usar template literals
+    Função 1: Escreva uma função que substitua a letra ‘x’ em uma frase.
+    O nome da função deverá ser substituaX.
+    A função deverá receber um nome por parâmetro.
+    Declare dentro da função uma variável do tipo const, com o nome frase, atribuindo o valor 'Tryber x aqui!'.
+    A função deverá retornar uma nova frase em que o x da frase 'Tryber x aqui!' seja substituído pelo nome passado por parâmetro.
+Exemplo:
+        Parâmetro: ‘Bebeto’
+        Retorno: ‘Tryber Bebeto aqui!’
+        Spoiler: O método split() pode ser utilizado de diferentes maneiras, por exemplo para separar as palavras de um texto.
+ */
+const changePhrase = (input) => {
+    const phrase = `Tryber x aqui!`;
+    console.log(phrase.replace("x", input));
+    return phrase;
+};
+
+changePhrase("Bebeto");
+
+/* 
+Função 2: Escreva uma função que receberá o retorno da Função 1 por parâmetro e retornará uma nova string.
+    O nome da função deverá ser minhasSkills.
+    A função deverá receber o retorno da Função 1 - substituaX por parâmetro.
+    Declare dentro da função uma variável com o nome skills e do tipo const.
+    A variável skills deverá ser um array que contenha três strings com tecnologias que você já estudou.
+    Crie uma varável do tipo let e concatene o valor retornado da Função 1 - substituaX (utilize template literals), a frase 'Minhas três principais habilidades são:' e o valor da variável skills.
+
+Exemplo de retorno:
+        Tryber Bebeto aqui!
+        Minhas três principais habilidades são:
+        JavaScript
+        HTML
+        CSS
+ */
+const minhasSkills = (changePhrase) => {
+    const skills = ["Javascript", "HTML", "CSS"];
+    const name = changePhrase;
+    console.log(`Tryber ${name} aqui!
+    Minhas três principais habilidades são:
+    - ${skills[0]}
+    - ${skills[1]}
+    - ${skills[2]}`);
+};
+minhasSkills("Bebeto");
