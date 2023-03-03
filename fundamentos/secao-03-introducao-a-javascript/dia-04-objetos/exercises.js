ac2ac5de7982f681a185e23c2ec5a95fc5477f64
Mariana Correa <mpcs270391@gmail.com>
2023-03-03 14:26:16 -0300

    Starts exercise bonus2

--- fundamentos/secao-03-introducao-a-javascript/dia-04-objetos/exercises.js
+++ fundamentos/secao-03-introducao-a-javascript/dia-04-objetos/exercises.js
@@ -131,7 +131,7 @@ function orderModifier(order) {
 
 orderModifier(order);
  */
-//Organizando lições
+/* //Organizando lições
 
 let lesson1 = {
     materia: "Matemática",
@@ -222,3 +222,55 @@ function verifyPropertie(obj, key, value) {
 }
 console.log(verifyPropertie(lesson3, "turno", "noite"));
 console.log(verifyPropertie(lesson3, "materia", "Maria Clara"));
+ */
+
+//Exercícios – Bônus
+/* 
+Exercício 1
+(Bônus) Faça um programa que receba uma string em algarismos romanos e retorne o número que a string representa.
+
+Atenção! Esse exercício já apareceu no processo seletivo de uma grande multinacional!
+
+Dicas:
+
+    Uma string é um array de caracteres, então cada elemento do array é uma letra.
+    O valor de cada numeral romano é:
+    | I   | 1    |
+| --- | ---- |
+| IV  | 4    |
+| V   | 5    |
+| IX  | 9    |
+| X   | 10   |
+| XL  | 40   |
+| L   | 50   |
+| XC  | 90   |
+| C   | 100  |
+| CD  | 400  |
+| D   | 500  |
+| CM  | 900  |
+| M   | 1000 |
+*/
+let dictionary = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+};
+
+function convertToNumber(string) {
+    string = string.toUpperCase().split("");
+    let number = 0;
+    for (let i of string) {
+        if (string[i] > string[i + 1]) {
+            number = +Object.values(string)[i];
+        } else {
+            number = -Object.values(string)[i];
+        }
+    }
+    return number;
+}
+
+console.log(convertToNumber("XIV"));
