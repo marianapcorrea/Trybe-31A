f259db94b1f846b0f2c5da2515a35cb0f0742a0d
Mariana Correa <mpcs270391@gmail.com>
2023-03-02 22:59:29 -0300

    Exercise 2

--- fundamentos/secao-03-introducao-a-javascript/dia-04-objetos/contentExercises.js
+++ fundamentos/secao-03-introducao-a-javascript/dia-04-objetos/contentExercises.js
@@ -21,3 +21,26 @@ console.log(`A jogadora Marta Silva foi eleita a melhor do mundo por ${player.be
 console.log(
     `A jogadora possui ${player.medals.golden} medalhas de ouro e ${player.medals.silver}  medalhas de prata`
 );
+*/
+
+//FOR/IN
+
+//Com base no objeto a seguir, utilize for/in e imprima no console a mensagem 'Olá, xxxxx' para cada nome, substituindo o xxxxx pelo nome em questão.
+let names = {
+    person1: "João",
+    person2: "Maria",
+    person3: "Jorge",
+};
+for (keys in names) {
+    console.log(`Olá, ${names[keys]}`);
+}
+
+//Usando o objeto abaixo, utilize for/in e imprima um console.log com as chaves e valores desse objeto.
+let car = {
+    model: "A3 Sedan",
+    manufacturer: "Audi",
+    year: 2020,
+};
+for (keys in car) {
+    console.log(keys, car[keys]);
+}
