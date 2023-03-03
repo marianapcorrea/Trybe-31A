b2c279c79b16093aa83aa314e0e818ac0c08b1df
Mariana Correa <mpcs270391@gmail.com>
2023-03-02 23:56:54 -0300

    Exercise Pedido de Clientes

--- fundamentos/secao-03-introducao-a-javascript/dia-04-objetos/exercises.js
+++ fundamentos/secao-03-introducao-a-javascript/dia-04-objetos/exercises.js
@@ -40,7 +40,7 @@ for (key in info) {
 }
  */
 
-//Leitura de Objetos
+/* //Leitura de Objetos
 let leitor = {
     nome: "Julia",
     sobrenome: "Pessoa",
@@ -66,4 +66,65 @@ leitor.livrosFavoritos.push({
     editora: "Rocco",
 });
 
-console.log(`Julia tem ${leitor.livrosFavoritos.length} livros favoritos`);
+console.log(`Julia tem ${leitor.livrosFavoritos.length} livros favoritos`); */
+
+//Pedido de clientes
+//Imagine que você seja responsável por cuidar do sistema de entrega de um restaurante e que precise enviar mensagens com as informações da compra. Para isso, use o seguinte código:
+let order = {
+    name: "Rafael Andrade",
+    phoneNumber: "11-98763-1416",
+    address: {
+        street: "Rua das Flores",
+        number: "389",
+        apartment: "701",
+    },
+    order: {
+        pizza: {
+            marguerita: {
+                amount: 1,
+                price: 25,
+            },
+            pepperoni: {
+                amount: 1,
+                price: 20,
+            },
+        },
+        drinks: {
+            coke: {
+                type: "Coca-Cola Zero",
+                price: 10,
+                amount: 1,
+            },
+        },
+        delivery: {
+            deliveryPerson: "Ana Silveira",
+            price: 5,
+        },
+    },
+    payment: {
+        total: 60,
+    },
+};
+function customerInfo(order) {
+    let buyer = "Ana Silveira";
+    console.log(
+        `Olá, ${buyer}, entrega para: ${order.name}, Telefone: ${order.phoneNumber}, R. ${order.address.street}, ${order.address.number}, AP: ${order.address.apartment}`
+    );
+}
+
+customerInfo(order);
+
+function orderModifier(order) {
+    let buyer = (order.name = "Luiz Silva");
+    let value = (order.payment.total = 50);
+    console.log(
+        `Olá, ${buyer}, o valor total de seu pedido de marguerita, pepperoni e Coca-Cola Zero é R$ ${value},00.`
+    );
+}
+
+orderModifier(order);
+//Complete a função customerInfo() para que seu retorno seja similar a 'Olá, Ana Silveira, entrega para: Rafael Andrade, Telefone: 11-98763-1416, R. Rua das Flores, Nº: 389, AP: 701'.
+
+//Complete a função orderModifier() para que seu retorno seja similar a 'Olá, Luiz Silva, o valor total de seu pedido de marguerita, pepperoni e Coca-Cola Zero é R$ 50,00.';
+//              Modifique o nome da pessoa compradora para Luiz Silva;
+//              Modifique o valor total da compra para R$ 50,00.
