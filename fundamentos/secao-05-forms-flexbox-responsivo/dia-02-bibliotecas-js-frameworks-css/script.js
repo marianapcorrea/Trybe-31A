34002666406802e0f3c1abb911c6599f81365413
Mariana Correa <mpcs270391@gmail.com>
2023-03-19 10:19:06 -0300

    Finish exercise

---
+++ fundamentos/secao-05-forms-flexbox-responsivo/dia-02-bibliotecas-js-frameworks-css/script.js
@@ -0,0 +1,39 @@
+const popoverTriggerList = document.querySelectorAll(
+  '[data-bs-toggle="popover"]'
+);
+const popoverList = [...popoverTriggerList].map(
+  (popoverTriggerEl) => new bootstrap.Popover(popoverTriggerEl)
+);
+
+const sendBtn = document.getElementById('submit-btn');
+let agreement = document.getElementById('agreement');
+const sendBtnSpan = document.querySelector('span');
+const inputs = document.querySelectorAll('input');
+const textArea = document.querySelector('textarea');
+const clearButton = document.querySelector('#clear-btn')
+
+const clearFields = () =>{
+textArea.value = '';
+for (let input of inputs){
+input.type === "text" ||input.type === "text" ? input.value = "" : input.value = false
+}
+}
+
+//
+window.onload = () => {
+  sendBtnSpan.addEventListener('mouseover', (event) => {
+    event.preventDefault();
+    if (sendBtn.hasAttribute('disabled') === true)
+      alert(
+        'Você precisa concordar com o uso das suas imagens para enviar o formulário'
+      );
+  });
+  sendBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+    alert('Formulário enviado! Boa sorte!');
+  });
+  agreement.addEventListener('change', () => {
+    if (agreement.checked) sendBtn.removeAttribute('disabled');
+  });
+clearButton.addEventListener("click", clearFields)
+};
