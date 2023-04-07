40517e826a17d37465afcb968c0b5f0ef4a507bc
Mariana Correa <mpcs270391@gmail.com>
2023-04-06 22:32:54 -0300

    Update readme

---
+++ fundamentos/secao-07-HoFs/dia-03-HoFs-filter-reduce/exercise.js
@@ -0,0 +1,119 @@
+// Fonte: <https://restcountries.com/v2/all>
+const countries = [
+  {
+    name: 'Afghanistan',
+    region: 'Asia',
+    currencies: [{ code: 'AFN', name: 'Afghan afghani' }],
+    capital: 'Kabul',
+    population: 40218234,
+    area: 652230
+  },
+  {
+    name: 'Åland Islands',
+    region: 'Europe',
+    currencies: [{ code: 'EUR', name: 'Euro' }],
+    capital: 'Mariehamn',
+    population: 28875,
+    area: 1580
+  },
+  {
+    name: 'Albania',
+    region: 'Europe',
+    currencies: [{ code: 'ALL', name: 'Albanian lek' }],
+    capital: 'Tirana',
+    population: 2837743,
+    area: 28748
+  },
+  {
+    name: 'Algeria',
+    region: 'Africa',
+    currencies: [{ code: 'DZD', name: 'Algerian dinar' }],
+    capital: 'Algiers',
+    population: 44700000,
+    area: 2381741
+  },
+  {
+    name: 'American Samoa',
+    region: 'Oceania',
+    currencies: [{ code: 'USD', name: 'United States Dollar' }],
+    capital: 'Pago Pago',
+    population: 55197,
+    area: 199
+  },
+  {
+    name: 'Andorra',
+    region: 'Europe',
+    currencies: [{ code: 'EUR', name: 'Euro' }],
+    capital: 'Andorra la Vella',
+    population: 77265,
+    area: 468
+  },
+  {
+    name: 'Angola',
+    region: 'Africa',
+    currencies: [{ code: 'AOA', name: 'Angolan kwanza' }],
+    capital: 'Luanda',
+    population: 32866268,
+    area: 1246700
+  },
+  {
+    name: 'Anguilla',
+    region: 'Americas',
+    currencies: [{ code: 'XCD', name: 'East Caribbean dollar' }],
+    capital: 'The Valley',
+    population: 13452,
+    area: 91
+  }
+];
+
+// 1 - Calcule a quantidade total da população de todos os países.
+// const expectedResult = 120797034;
+const getPopulation = () => {
+ // retorne o seu código aqui
+}
+
+// 2 - Calcule a área total de todos os países.
+// const expectedResult = 4311757;
+const getTotalArea = () => {
+// retorne seu código aqui
+}
+
+//🚀 3 - Encontre o país com o maior nome.
+// const expectedResult = {
+  //   name: 'American Samoa',
+  //   region: 'Oceania',
+  //   currencies: [{ code: 'USD', name: 'United States Dollar' }],
+  //   capital: 'Pago Pago',
+  //   population: 55197,
+  //   area: 199
+// }
+const longestName = () => {
+  // retorne seu código aqui
+}
+
+// 🚀 4 - Retorne a quantidade de vezes que a letra a maiúscula ou minúscula aparece no array de nomes.
+const names = [
+  'Aanemarie', 'Adervandes', 'Akifusa',
+  'Abegildo', 'Adicellia', 'Aladonata',
+  'Abeladerco', 'Adieidy', 'Alarucha',
+];
+// const expectedResult = 20;
+const countA = () => {
+  // retorne seu código aqui
+}
+
+// 🚀 5 - Crie um array de objetos e calcule a média de notas. Utilize as constantes students e grades para criar um array de objetos e calcule a média da nota das pessoas estudantes:
+// O index 0 do array `students` equivale ao index 0 do array `grades`
+const students = ['Pedro Henrique', 'Miguel', 'Maria Clara'];
+const grades = [[9, 8, 10, 7, 5], [10, 9, 9, 10, 8], [10, 7, 10, 8, 9]];
+//  const expectedResult = [
+  //  { name: 'Pedro Henrique', average: 7.8 },
+  //  { name: 'Miguel', average: 9.2 },
+  //  { name: 'Maria Clara', average: 8.8 },
+// ];
+const studentAverage = () => {
+  // retorne seu código aqui
+}
+
+
+
---
+++ fundamentos/secao-07-HoFs/dia-03-HoFs-filter-reduce/reduce.js
@@ -0,0 +1,97 @@
+//Tente criar a função que busca o maior valor do array numbers utilizando apenas o reduce. 
+const numbers = [50, 85, -30, 3, 15];
+
+//console.log(numbers.reduce((acc, curr)=> acc > curr ? acc : curr))
+
+//1 - Para fixar ainda mais o conceito de reduce, faça uma função que some todos os números pares do array numbers. Tente criar duas funções, uma usando reduce e filter, e outra apenas usando reduce.
+const numbers2 = [18, 19, 23, 53, 4, 5, 76, 23, 54];
+
+ console.log(numbers2.reduce((acc, curr)=> (curr %2 === 0) ? acc + curr : acc));
+ 
+const makeSum = (a,b) =>(b%2 ===0 ) ? a+b : a
+ console.log(numbers2.reduce(makeSum))
+
+// 2 - Crie uma função usando dados de estudantes, para mostrar na tela um relatório que diz em qual matéria a pessoa foi melhor. Você usará tanto o map quanto o reduce dentro dele!
+const estudantes = [
+  {
+    nome: 'Jorge',
+    sobrenome: 'Silva',
+    idade: 14,
+    turno: 'Manhã',
+    materias: [
+      { name: 'Matemática', nota: 67 },
+      { name: 'Português', nota: 79 },
+      { name: 'Química', nota: 70 },
+      { name: 'Biologia', nota: 65 },
+    ],
+  },
+  {
+    nome: 'Mario',
+    sobrenome: 'Ferreira',
+    idade: 15,
+    turno: 'Tarde',
+    materias: [
+      { name: 'Matemática', nota: 59 },
+      { name: 'Português', nota: 80 },
+      { name: 'Química', nota: 78 },
+      { name: 'Biologia', nota: 92 },
+    ],
+  },
+  {
+    nome: 'Jorge',
+    sobrenome: 'Santos',
+    idade: 15,
+    turno: 'Manhã',
+    materias: [
+      { name: 'Matemática', nota: 76 },
+      { name: 'Português', nota: 90 },
+      { name: 'Química', nota: 70 },
+      { name: 'Biologia', nota: 80 },
+    ],
+  },
+  {
+    nome: 'Maria',
+    sobrenome: 'Silveira',
+    idade: 14,
+    turno: 'Manhã',
+    materias: [
+      { name: 'Matemática', nota: 91 },
+      { name: 'Português', nota: 85 },
+      { name: 'Química', nota: 92 },
+      { name: 'Biologia', nota: 90 },
+    ],
+  },
+  {
+    nome: 'Natalia',
+    sobrenome: 'Castro',
+    idade: 14,
+    turno: 'Manhã',
+    materias: [
+      { name: 'Matemática', nota: 70 },
+      { name: 'Português', nota: 70 },
+      { name: 'Química', nota: 60 },
+      { name: 'Biologia', nota: 50 },
+    ],
+  },
+  {
+    nome: 'Wilson',
+    sobrenome: 'Martins',
+    idade: 14,
+    turno: 'Manhã',
+    materias: [
+      { name: 'Matemática', nota: 80 },
+      { name: 'Português', nota: 82 },
+      { name: 'Química', nota: 79 },
+      { name: 'Biologia', nota: 75 },
+    ],
+  },
+];
+
+const getBetterClass = () => {
+  return estudantes.map((student)=>({
+    name: student.nome,
+    materia: student.materias.reduce((acc, curr)=> acc.nota > curr.nota  ? acc : curr ).name
+  }))
+}
+
+console.log(getBetterClass())
